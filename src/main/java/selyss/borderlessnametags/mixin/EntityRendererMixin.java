cdd57c5071de47b3b5cd80d935a59af759e6c237
Selyss <99344963+Selyss@users.noreply.github.com>
2024-08-07 16:19:39 -0400

    working config for color

--- src/main/java/selyss/borderlessnametags/mixin/EntityRendererMixin.java
+++ src/main/java/selyss/borderlessnametags/mixin/EntityRendererMixin.java
@@ -11,6 +11,8 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import selyss.borderlessnametags.ColorUtils;
 import selyss.borderlessnametags.manager.ConfigManager;
 
+import java.awt.*;
+
 @Mixin(EntityRenderer.class)
 public class EntityRendererMixin {
 	@Redirect(method = "renderLabelIfPresent", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/text/Text;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;II)I"))
@@ -23,8 +25,9 @@ public class EntityRendererMixin {
 			return textRenderer.draw(text, x, y, color, shadow, matrix, vertexConsumers, layerType, backgroundColor, light);
 		}
 
-		int textColor = ColorUtils.colorToARGB(ConfigManager.getEnabledTextColor());
+		Color textColor = ConfigManager.getEnabledTextColor();
+		int argbColor = ColorUtils.colorToARGB(textColor);
 
-		return textRenderer.draw(text, x, y, textColor, true, matrix, vertexConsumers, TextRenderer.TextLayerType.NORMAL, 0x00FFFFFF, light);
+		return textRenderer.draw(text, x, y, argbColor, true, matrix, vertexConsumers, TextRenderer.TextLayerType.NORMAL, 0x00FFFFFF, light);
 	}
 }
